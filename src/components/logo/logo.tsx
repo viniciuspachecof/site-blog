3ccb5945dcbb4986985f6042ce0360ce87b662cf
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-02-23 22:25:37 -0300

    Componente header, active-link e footer. Criação laytout

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Logo = () => {
+  return (
+    <Link href="/" title="página inicial">
+      <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+    </Link>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
-  return (
-    <div>
-      <h2 className="text-4xl text-red-300">title</h2>
-
-      <Button variant="destructive">Button</Button>
-    </div>
-  );
+  return <div></div>;
 }
