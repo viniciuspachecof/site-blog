50072cea7c1de67f1ac1b069f7d27fba62d723f5
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-02-24 22:18:21 -0300

    Adicionado Style guide, seção hero e feature

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -8,15 +8,14 @@ type ActiveLinkProps = {
 
 export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
   const router = useRouter();
-  const isCurrentPath =
-    router.asPath === href || router.asPath === rest.as || router.asPath.startsWith(String(rest.as));
+  const isCurrentPath = router.asPath === href || router.asPath === rest.as;
 
   return (
     <Link
       href={href}
       className={cn(
-        'text-sm font-medium transition-colors hover:text-blue-500',
-        isCurrentPath ? 'text-blue-500' : 'text-muted-foreground',
+        'text-action-sm transition-colors hover:text-blue-500',
+        isCurrentPath ? 'text-blue-500' : 'text-gray-100',
       )}
     >
       {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { FeatureSection } from '@/components/feature-section';
+import { HeroSection } from '@/components/hero-section';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+        <FeatureSection />
+      </article>
+    </>
+  );
 }
