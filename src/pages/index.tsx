883a30e79c5d88a91049d0f56597c393052a6163
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-02 22:41:39 -0300

    Implementação do contentlayer e criando página de blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { LandingPage } from '@/templates/landing-page';
 
+import { allPosts } from 'contentlayer/generated';
+
 export default function Home() {
+  console.log(allPosts);
+
   return <LandingPage />;
 }
