3f003328872c80e12abdc96623f09e2f834a3102
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-03 22:15:42 -0300

    Componente de Search e PostCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import { LandingPage } from '@/templates/landing-page';
 
-import { allPosts } from 'contentlayer/generated';
-
 export default function Home() {
-  console.log(allPosts);
-
   return <LandingPage />;
 }
