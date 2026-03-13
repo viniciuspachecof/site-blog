c4c6db0e94dbfe422a1af560bbf858300c21e64e
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-12 22:41:39 -0300

    Migrando para App router

--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -1,21 +1,24 @@
+'use client';
 import { cn } from '@/lib/utils';
 import Link, { LinkProps } from 'next/link';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 
 type ActiveLinkProps = {
   children: React.ReactNode;
 } & LinkProps;
 
 export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
-  const router = useRouter();
-  const isCurrentPath = router.asPath === href || router.asPath === rest.as;
+  const linkPath = (typeof href === 'string' ? href : href.pathname) ?? '';
+  const pathname = usePathname();
+  const isActive = pathname === linkPath || pathname?.startsWith(`${linkPath}/`);
 
   return (
     <Link
+      {...rest}
       href={href}
       className={cn(
         'text-action-sm transition-colors hover:text-blue-500',
-        isCurrentPath ? 'text-blue-500' : 'text-gray-100',
+        isActive ? 'text-blue-500' : 'text-gray-100',
       )}
     >
       {children}
--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
+'use client';
 import { Search } from '@/components/search';
-import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
 import { PostCard } from './components/post-card';
 import { PostGridCard } from './components/post-grid-card';
 import { Post } from 'contentlayer/generated';
@@ -10,8 +11,8 @@ export type BlogListProps = {
 };
 
 export function BlogList({ posts }: BlogListProps) {
-  const router = useRouter();
-  const query = router.query.q as string;
+  const searchParams = useSearchParams();
+  const query = searchParams?.get('q') ?? '';
   const pageTitle = query
     ? `Resultados de buscara para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio';
