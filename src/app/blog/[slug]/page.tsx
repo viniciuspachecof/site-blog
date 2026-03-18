a3eca551d74321a6c2217d80e6d202c65588a70e
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-17 21:31:24 -0300

    chore: add seo in blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,32 @@
+import type { Metadata } from 'next';
 import { PostPage } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
 import { notFound } from 'next/navigation';
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export const revalidate = 60;
 
@@ -16,7 +36,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import '@/styles/globals.css';
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://site-blog-psi.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-psi.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
