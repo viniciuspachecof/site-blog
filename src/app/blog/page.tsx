faac2088a65952de3303a105aa534179f0a2f52a
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-17 21:21:27 -0300

    chore: add seo in blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
+import { Metadata } from 'next';
 import { BlogList } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://site-blog-psi.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://site-blog-psi.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
